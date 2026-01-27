449b20fad08c74f3b78f05e25fadf4406d30418f
rohith <rohith@rohiths-MacBook-Air.local>
2026-01-27 11:12:17 +0530

    added some programmes

--- hello.py
+++ hello.py
@@ -1229,5 +1229,71 @@ while num>0:
     num //=10
     print(rev)
     
+num=int(input("enter a number"))
+rev=0
+while num>0:
+    rev=rev*10 + num %10
+    num //=10
+    print(rev)
 
 
+num=int(input("enter a number"))
+rev=0
+while num>0:
+    rev=rev*10 + num %10
+    num //=10
+    print(rev)
+
+
+num=int(input("enter a number"))
+rev=0
+while num>0:
+    rev=rev*10 + num %10
+    num //=10
+    print(rev)
+    
+    
+num=int(input("enter a number"))
+rev=0
+while num>0:
+    rev=rev*10 + num %10
+    num //=10
+    print(rev)
+
+num=int(input("enter a number"))
+rev=0
+while num>0:
+    rev=rev*10 + num %10
+    num //=10
+    print(rev)
+
+
+num=int(input("enter a number"))
+rev=0
+while num>0:
+    rev=rev*10 + num%10
+    num //=10
+    print(rev)
+
+num=int(input("enter a number"))
+rev=0
+while num>0:
+    rev=rev*10 + num %10
+    num //=10
+    print(rev)
+    
+    
+num=int(input("enter number"))
+rev=0
+while num>0:
+    rev=rev*10 +num %10
+    num //=10
+    print(rev)
+
+
+num=int(input("enter a number"))
+rev=0
+while num>0:
+    rev=rev*10 +num %10
+    num //=10
+    
